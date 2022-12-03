241ce7d14e84b4de3f00c9c60834eb7b3559ab8a
Robert Ellis <rob@pigatron-industries.co.uk>
2022-12-03 16:22:24 +0000

    Add hardware pin defs

--- src/Hardware.h
+++ src/Hardware.h
@@ -27,8 +27,8 @@ class Hardware {
 
         #define AnalogInputPinT AnalogInputPin<NativeDevice>
 
-        AnalogInput(cv1Pin, A10)
-        AnalogInput(cv2Pin, A11)
+        AnalogInput(cv1Pin, CV1_PIN)
+        AnalogInput(cv2Pin, CV2_PIN)
         AnalogInputPin<> cv1Input = AnalogInputPin<>(cv1Pin);
         AnalogInputPin<> cv2Input = AnalogInputPin<>(cv2Pin);
 
@@ -37,11 +37,11 @@ class Hardware {
         IS32FL3738Device is32fl3738 = IS32FL3738Device(Wire, SDB_PIN);
         AnalogOutputPin<IS32FL3738Device>* matrixLeds[6][6] = {
             { &is32fl3738.pins[0], &is32fl3738.pins[1], &is32fl3738.pins[2], &is32fl3738.pins[3], &is32fl3738.pins[4], &is32fl3738.pins[5]},
-            { &is32fl3738.pins[0], &is32fl3738.pins[1], &is32fl3738.pins[2], &is32fl3738.pins[3], &is32fl3738.pins[4], &is32fl3738.pins[5]},
-            { &is32fl3738.pins[0], &is32fl3738.pins[1], &is32fl3738.pins[2], &is32fl3738.pins[3], &is32fl3738.pins[4], &is32fl3738.pins[5]},
-            { &is32fl3738.pins[0], &is32fl3738.pins[1], &is32fl3738.pins[2], &is32fl3738.pins[3], &is32fl3738.pins[4], &is32fl3738.pins[5]},
-            { &is32fl3738.pins[0], &is32fl3738.pins[1], &is32fl3738.pins[2], &is32fl3738.pins[3], &is32fl3738.pins[4], &is32fl3738.pins[5]},
-            { &is32fl3738.pins[0], &is32fl3738.pins[1], &is32fl3738.pins[2], &is32fl3738.pins[3], &is32fl3738.pins[4], &is32fl3738.pins[5]}
+            { &is32fl3738.pins[6], &is32fl3738.pins[7], &is32fl3738.pins[8], &is32fl3738.pins[9], &is32fl3738.pins[10], &is32fl3738.pins[11]},
+            { &is32fl3738.pins[12], &is32fl3738.pins[13], &is32fl3738.pins[14], &is32fl3738.pins[15], &is32fl3738.pins[16], &is32fl3738.pins[17]},
+            { &is32fl3738.pins[18], &is32fl3738.pins[19], &is32fl3738.pins[20], &is32fl3738.pins[21], &is32fl3738.pins[22], &is32fl3738.pins[23]},
+            { &is32fl3738.pins[24], &is32fl3738.pins[25], &is32fl3738.pins[26], &is32fl3738.pins[27], &is32fl3738.pins[28], &is32fl3738.pins[29]},
+            { &is32fl3738.pins[30], &is32fl3738.pins[31], &is32fl3738.pins[32], &is32fl3738.pins[33], &is32fl3738.pins[34], &is32fl3738.pins[35]}
         };
 
         // OLEDDisplay display;
--- src/hwconfig.h
+++ src/hwconfig.h
@@ -1,15 +1,25 @@
 
 // Teensy Pins
-#define ENCODER_BTN_PIN 2
-#define ENCODER_PIN1 3
-#define ENCODER_PIN2 4
-#define GATE_OUTPUT_PIN 5
-#define RANGE_SWITCH_PIN 6
+#define ENCODER_BTN_PIN 32
+#define ENCODER_PIN1 31
+#define ENCODER_PIN2 30
+#define CV1_PIN A9
+#define CV2_PIN A8
 
-#define SDB_PIN 7
+#define SDB_PIN 17
 #define SCL_PIN 19
 #define SDA_PIN 18
 #define I2C_SPEED 1000000
+
+#define POT1_CS 9
+#define POT2_CS 8
+#define POT3_CS 7
+#define POT4_CS 6
+#define POT5_CS 5
+#define POT6_CS 4
+#define POT7_CS 3
+#define POT8_CS 2
+#define POT9_CS 1
 
 // Other config
 #define SERIAL_BAUD 115200
